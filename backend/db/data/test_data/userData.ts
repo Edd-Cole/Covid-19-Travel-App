d3a144c1c9c4e677d66551381542d3c424904659
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-13 11:52:35 +0100

    Creating test user data

---
+++ backend/db/data/test_data/userData.ts
@@ -0,0 +1,130 @@
+module.exports = [
+    {
+        fullName: "John Smith",
+        email: "js@google.com",
+        password: "password",
+        trips: [{
+            country: "france",
+            trafficLight: "amber",
+            dateGoing: "2022.01.12",
+            dateReturning: "2022.01.24",
+            acceptingTourists: true,
+            vaccineRequired: true,
+            testRequired: true,
+            extraDocsRequired: true,
+            newInfo: false
+        },
+        {
+        country: "greece",
+        trafficLight: "amber",
+        dateGoing: "2022.05.03",
+        dateReturning: "2022.05.10",
+        acceptingTourists: true,
+        vaccineRequired: true,
+        testRequired: true,
+        extraDocsRequired: true,
+        newInfo: true
+        }],
+        pastTrips: [{
+            country: "poland",
+            dateGoing: "2021.12.02",
+            dateReturning: "2021.12.06",
+        }],
+    },
+    {
+        fullName: "Edd Kleszcz",
+        email: "ek@sadballoons.com",
+        password: "thisIsTheBestPasswordEver",
+        trips: [{
+            country: "spain",
+            trafficLight: "amber",
+            dateGoing: "2022.01.12",
+            dateReturning: "2022.01.24",
+            acceptingTourists: true,
+            vaccineRequired: true,
+            testRequired: true,
+            extraDocsRequired: true,
+            newInfo: true
+        },
+        {
+            country: "ireland",
+            trafficLight: "amber",
+            dateGoing: "2022.05.03",
+            dateReturning: "2022.05.10",
+            acceptingTourists: true,
+            vaccineRequired: true,
+            testRequired: true,
+            extraDocsRequired: true,
+            newInfo: false
+        },
+        {
+            country: "netherlands",
+            trafficLight: "amber",
+            dateGoing: "2022.05.011",
+            dateReturning: "2022.05.19",
+            acceptingTourists: true,
+            vaccineRequired: true,
+            testRequired: true,
+            extraDocsRequired: true,
+            newInfo: false
+        }],
+        pastTrips: [{
+            country: "italy",
+            dateGoing: "2021.12.02",
+            dateReturning: "2021.12.06",
+        },
+        {
+            country: "france",
+            dateGoing: "2021.12.07",
+            dateReturning: "2021.12.10",
+        }],
+    },
+    {
+        fullName: "Mahamud Arteh",
+        email: "ma@sadballoons.com",
+        password: "IrockThePARTY",
+        trips: [{
+            country: "spain",
+            trafficLight: "amber",
+            dateGoing: "2022.01.12",
+            dateReturning: "2022.01.24",
+            acceptingTourists: true,
+            vaccineRequired: true,
+            testRequired: true,
+            extraDocsRequired: true,
+            newInfo: true
+        },
+        {
+            country: "ireland",
+            trafficLight: "amber",
+            dateGoing: "2022.05.03",
+            dateReturning: "2022.05.10",
+            acceptingTourists: true,
+            vaccineRequired: true,
+            testRequired: true,
+            extraDocsRequired: true,
+            newInfo: false
+        },
+        {
+            country: "netherlands",
+            trafficLight: "amber",
+            dateGoing: "2022.05.011",
+            dateReturning: "2022.05.19",
+            acceptingTourists: true,
+            vaccineRequired: true,
+            testRequired: true,
+            extraDocsRequired: true,
+            newInfo: false
+        }],
+        pastTrips: [{
+            country: "italy",
+            dateGoing: "2021.12.02",
+            dateReturning: "2021.12.06",
+        },
+        {
+            country: "france",
+            dateGoing: "2021.12.07",
+            dateReturning: "2021.12.10",
+        }],
+    }
+]
