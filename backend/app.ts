588c570c5f12584910bb6b1674fa101583a7e80b
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-14 11:13:09 +0100

    building app

---
+++ backend/app.ts
@@ -0,0 +1,11 @@
+const express = require('express');
+const apiRouter = require('./routers/api-router');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api');
+
+export default app;
