0c3cc555f568f77af6712ba174dc5d187930cf7a
Mahamud Arteh <artehrush@gmail.com>
2021-09-14 10:13:39 +0100

    seed function updated, userData added

--- backend/db/seeds/seed.ts
+++ backend/db/seeds/seed.ts
@@ -2,26 +2,30 @@ import main from "./connection";
 
 const seed = async (countryData: object[], userData: object[]) => {
   // 1. drop collections
-  return main().then(async (db) => {
-      await db.collection('countries').drop();
-      await db.collection('users').drop();
-    return db;
-  })
-  // 2. create collections
-  .then(async (db) => {
-      await db.createCollection('countries');
-      await db.createCollection('users');
-      return db;
-  })
-  // 3. populate collections - country data
-  .then(async (db) => {
-      await db.collection('countries').insertMany(countryData);
-      return db;
-    })
-  // 4. populate collections - user data
-  .then((db) => {
-    return db
-  })
-}
+  return (
+    main()
+      .then(async db => {
+        await db.collection("countries").drop();
+        await db.collection("users").drop();
+        return db;
+      })
+      // 2. create collections
+      .then(async db => {
+        await db.createCollection("countries");
+        await db.createCollection("users");
+        return db;
+      })
+      // 3. populate collections - country data
+      .then(async db => {
+        await db.collection("countries").insertMany(countryData);
+        return db;
+      })
+      // 4. populate collections - user data
+      .then(async db => {
+        await db.collection("users").insertMany(userData);
+        return db;
+      })
+  );
+};
 
 export default seed;
