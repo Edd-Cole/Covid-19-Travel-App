7364ce53ef3ab3294eb64fb48dcb0292bfcadefe
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-15 15:06:44 +0100

    Adding logic with notes for endpoint /users/:email - POST

--- backend/models/users.models.ts
+++ backend/models/users.models.ts
@@ -1,12 +1,16 @@
 const { main: mongoCl} = require('../db/seeds/connection');
 
 const fetchUser = (email: string, password: string) => {
+    ///connect to database
     return mongoCl()
     .then((db: any) => {
+        //connect to the relevant collection and find the user given their email address
         return db.collection("users")
         .findOne({email: email})
-        .then((user: object) => {
-            console.log(user)
+        .then((user: any) => {
+            //before returning, remove password to keep user secure
+            delete user.password;
+            return user
     })
   })
 }
