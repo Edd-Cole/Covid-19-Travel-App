7b6f8447ca8437cc04481b7061474b04e9a71468
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-15 14:44:31 +0100

    adding users router to api-router

--- backend/routers/api-router.ts
+++ backend/routers/api-router.ts
@@ -3,8 +3,10 @@ const {
   getEndpoints: grabEndpoints,
 } = require("../controllers/controllers.api");
 const { countriesRouter: apiCountries } = require("./countries-router");
+const { usersRouter: apiUsers} = require("./users-router");
 
 apiRouter.get("/", grabEndpoints);
 apiRouter.use("/countries", apiCountries);
+apiRouter.use("/users", apiUsers);
 
 module.exports = { apiRouter };
