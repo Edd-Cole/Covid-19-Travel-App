d69881f0f5a9f1e510eb3d975c00ffd1e17ac457
Mahamud Arteh <artehrush@gmail.com>
2021-09-15 10:56:44 +0100

    Refactored error handler

--- backend/errors/error-handling.ts
+++ backend/errors/error-handling.ts
@@ -1,7 +1,5 @@
 export const customErrors = (err: any, req: any, res: any, next: any) => {
-    console.log(err)
-    if(err.code === 400) {
-        res.status(err.code).send(err)
-    }
-    next(err)
-}
+  console.log(err);
+  res.status(err.code).send(err);
+  next(err);
+};
