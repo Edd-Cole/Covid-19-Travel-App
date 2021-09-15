4f2dfa8bc565b423f1203184a6788754af736666
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-15 11:27:27 +0100

    resolving merge conflict

--- backend/errors/error-handling.ts
+++ backend/errors/error-handling.ts
@@ -1,7 +1,7 @@
 export const customErrors = (err: any, req: any, res: any, next: any) => {
-    console.log(err)
-    if(err.code === 400) {
-        res.status(err.code).send(err)
-    }
-    next(err)
-}
+  console.log(err);
+  if (err.code && err.msg) {
+    res.status(err.code).send(err);
+  }
+  next(err);
+};
