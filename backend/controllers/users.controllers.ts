cde4091e1eb1f77a9a67a46bbbc342803c2c875f
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-15 15:04:46 +0100

    Adding logic with notes for endpoint /users/:email

--- backend/controllers/users.controllers.ts
+++ backend/controllers/users.controllers.ts
@@ -5,7 +5,8 @@ export const setUser = (req: any, res: any, next: any) => {
     const { email, password } = req.body;
     return selectUser(email, password)
         .then((user: object) => {
-            console.log(user)
+            //Send user back in an object with key of user
+            res.status(200).send({user})
         })
         .catch((err: any) => {
             next(err)
