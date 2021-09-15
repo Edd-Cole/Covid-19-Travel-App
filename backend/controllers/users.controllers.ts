5de01e8844f666b7006a4a92e1865a43e921e1d7
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-15 14:55:33 +0100

    renaming function for exporting purposes

--- backend/controllers/users.controllers.ts
+++ backend/controllers/users.controllers.ts
@@ -1,6 +1,6 @@
 const { fetchUser: selectUser } = require('../models/users.models.ts');
 
-export const grabUser = (req: any, res: any, next: any) => {
+export const setUser = (req: any, res: any, next: any) => {
     //destructure email and password from the request body, then send into models
     const { email, password } = req.body;
     return selectUser(email, password)
