812049f415165acb088aa222c874cc16bccc6f25
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-15 10:16:54 +0100

    Building controller for endpoint /countries/:country

--- backend/controllers/countries.controllers.ts
+++ backend/controllers/countries.controllers.ts
@@ -1,10 +1,11 @@
 const {
   fetchCountries: selectCountries,
+  fetchCountry: selectCountry
 } = require("../models/countries.models");
 
-export const getCountries = async (req: any, res: any, next: any) => {
+export const getCountries = (req: object, res: any, next: any) => {
     // Invoke the models function for this controller
-  selectCountries()
+  return selectCountries()
     .then((countries: string[]) => {
         // send with status 200 and wrapped in an object called countries
       res.status(200).send({ countries });
@@ -14,3 +15,15 @@ export const getCountries = async (req: any, res: any, next: any) => {
       next(err)
     });
 };
+
+export const getCountry = (req: any, res: any, next: any) => {
+    const { country } = req.params;
+    return selectCountry(country)
+      .then((country: object) => {
+          res.status(200).send(country);
+      })
+      .catch((err: object) => {
+          console.log(err);
+          next(err);
+      })
+}
