50f37044ac908de39f368a7ae608245fc32bbbd0
CHARIOT_SEB <sebboyse@gmail.com>
2021-09-15 15:47:10 +0100

    error for user not found

--- backend/models/users.models.ts
+++ backend/models/users.models.ts
@@ -1,19 +1,22 @@
-const { main: mongoCl} = require('../db/seeds/connection');
+const { main: mongoCl } = require('../db/seeds/connection');
 
 const fetchUser = (email: string, password: string) => {
-    ///connect to database
-    return mongoCl()
-    .then((db: any) => {
-        //connect to the relevant collection and find the user given their email address
-        return db.collection("users")
-        .findOne({email: email})
-        .then((user: any) => {
-            //before returning, remove password to keep user secure
-            delete user._id;
-            delete user.password;
-            return user
-    })
-  })
-}
+	///connect to database
+	return mongoCl().then((db: any) => {
+		//connect to the relevant collection and find the user given their email address
+		return db
+			.collection('users')
+			.findOne({ email: email })
+			.then((user: any) => {
+				if (!user) {
+					return Promise.reject({ code: 404, msg: 'User not found' });
+				}
+				//before returning, remove password to keep user secure
+				delete user._id;
+				delete user.password;
+				return user;
+			});
+	});
+};
 
-module.exports = { fetchUser }
+module.exports = { fetchUser };
