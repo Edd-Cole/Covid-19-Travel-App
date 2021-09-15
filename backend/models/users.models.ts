d1e338a243e368191fd7e8b903cac99edea2a366
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-15 14:59:38 +0100

    Connecting successfully to mongo client

--- backend/models/users.models.ts
+++ backend/models/users.models.ts
@@ -2,11 +2,13 @@ const { main: mongoCl} = require('../db/seeds/connection');
 
 const fetchUser = (email: string, password: string) => {
     return mongoCl()
-    .collection("users")
-    .findOne({email: email})
-    .then((user: object) => {
-        console.log(user)
+    .then((db: any) => {
+        return db.collection("users")
+        .findOne({email: email})
+        .then((user: object) => {
+            console.log(user)
     })
+  })
 }
 
 module.exports = { fetchUser }
