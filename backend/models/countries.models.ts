8c6c0b62ac462548821ef56317564d95abda4fdd
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-15 09:51:40 +0100

    refactoring

--- backend/models/countries.models.ts
+++ backend/models/countries.models.ts
@@ -1,23 +1,21 @@
 const { main: mongo } = require("../db/seeds/connection");
 
-const fetchCountries = async () => {
+const fetchCountries = () => {
   // Establish connection to the database and, using promises, manipulate info
-  const db = await mongo().then(async (db: any) => {
+  return mongo().then((db: any) => {
       // Use info from the countries collection within DB, get all the documents inside
-    const countries = await db.collection("countries")
+    return db.collection("countries")
       .find({})
+      .sort({country: 1})
       .toArray()
       .then((countryData: any) => {
           //  map and return the capitalised version of the country
-        const countriesFromDB: string[] =  countryData.map((country: any) => {
-            const capitalisedCountry = country.country[0].toUpperCase() + country.country.slice(1)
+        return countryData.map((country: any) => {
+            const capitalisedCountry = country.country[0].toUpperCase() + country.country.slice(1);
             return capitalisedCountry;
         });
-        // sort into alphabetical order
-        return countriesFromDB.sort();
       });
-    return countries;
   });
-  return db;
 };
+
 module.exports = { fetchCountries };
