ee351a2b7a4f153ddf177b5b221755645a96aee7
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-15 14:57:54 +0100

    Exporting fetchUser

--- backend/models/users.models.ts
+++ backend/models/users.models.ts
@@ -2,9 +2,11 @@ const { main: mongoCl} = require('../db/seeds/connection');
 
 const fetchUser = (email: string, password: string) => {
     return mongoCl()
-    .collection('users')
+    .collection("users")
     .findOne({email: email})
     .then((user: object) => {
         console.log(user)
     })
 }
+
+module.exports = { fetchUser }
