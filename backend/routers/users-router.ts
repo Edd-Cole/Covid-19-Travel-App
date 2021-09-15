55f56b8e3d18510c7b65a17f865672e9d8889a21
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-15 16:33:39 +0100

    adding route for new endpoint

--- backend/routers/users-router.ts
+++ backend/routers/users-router.ts
@@ -1,6 +1,8 @@
 const usersRouter = require('express').Router();
 const { setUser: grabUser} = require('../controllers/users.controllers');
+const { postUser: createUser} = require('../controllers/users.controllers');
 
+usersRouter.route('/').post(createUser)
 usersRouter.route('/:email').post(grabUser);
 
 module.exports = { usersRouter }
