0447d6d1420f3077480e92fc40f395af1637ee3f
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-16 17:19:02 +0100

    Adding connection for hosted database

--- backend/db/seeds/connection.ts
+++ backend/db/seeds/connection.ts
@@ -3,25 +3,28 @@ const path = require("path");
 
 //Connection URL
 const url = "mongodb://localhost:27017";
-const client = new MongoClient(url);
+let client = new MongoClient(url);
 
 //Database name changes depending on whether tests are being run or not
 const ENV = process.env.NODE_ENV || "development";
 
-// if(ENV === "production") {
-    
-// }
 
 let dbSuffix = process.env.NODE_ENV || "";
 
 //adjust suffix if normal database connection isn't being established
 if (dbSuffix !== "") {
-  dbSuffix = "-" + dbSuffix;
+    dbSuffix = "-" + dbSuffix;
 }
 const dbName = `covid-travel-app${dbSuffix}`;
 require("dotenv").config({
-  path: path.resolve(__dirname, `../.env.${ENV}`),
+    path: path.resolve(__dirname, `../.env.${ENV}`),
 });
+
+//if current value for ENV is production, set the database to mongo cloud
+if(ENV === "production") {
+    const uri = "mongodb+srv://covid-travel-app:<password>@travel-app-data.1d4ta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+}
 
 //Establish database connection asynchronously
 const main = async () => {
@@ -31,13 +34,3 @@ const main = async () => {
 };
 
 module.exports = { client, main, url };
-
-
-// const { MongoClient } = require('mongodb');
-// const uri = "mongodb+srv://covid-travel-app:<password>@travel-app-data.1d4ta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
