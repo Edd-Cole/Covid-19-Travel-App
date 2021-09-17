cb35e703c2c6c65013ee236d3c12fab1c63fd4bc
HeatherBerrill <heather.berrill@btinternet.com>
2021-09-17 15:31:41 +0100

    creating endpoint for /countries/post

--- backend/controllers/countries.controllers.ts
+++ backend/controllers/countries.controllers.ts
@@ -1,28 +1,33 @@
 const {
   fetchCountries: selectCountries,
-  fetchCountry: selectCountry
-} = require("../models/countries.models");
+  fetchCountry: selectCountry,
+  insertCountry: createCountry
+} = require('../models/countries.models');
 
 export const getCountries = (req: object, res: any, next: any) => {
-    // Invoke the models function for this controller
+  // Invoke the models function for this controller
   return selectCountries()
     .then((countries: string[]) => {
-        // send with status 200 and wrapped in an object called countries
+      // send with status 200 and wrapped in an object called countries
       res.status(200).send({ countries });
     })
     .catch((err: object) => {
-        // Send error to error handling functions
-      next(err)
+      // Send error to error handling functions
+      next(err);
     });
 };
 
 export const getCountry = (req: any, res: any, next: any) => {
-    const { country } = req.params;
-    return selectCountry(country)
-      .then((country: object) => {
-          res.status(200).send({country});
-      })
-      .catch((err: object) => {
-          next(err);
-      })
-}
+  const { country } = req.params;
+  return selectCountry(country)
+    .then((country: object) => {
+      res.status(200).send({ country });
+    })
+    .catch((err: object) => {
+      next(err);
+    });
+};
+
+export const addCountry = (req: any, res: any, next: any) => {
+  return createCountry;
+};
