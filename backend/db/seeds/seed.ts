81d01e133a6881984a65151bbfc905b513b36814
Edd-Cole <edd_kleszcz@hotmail.co.uk>
2021-09-22 11:10:10 +0100

    refactoring

--- backend/db/seeds/seed.ts
+++ backend/db/seeds/seed.ts
@@ -6,16 +6,10 @@ const seed = async (countryData: object[], userData: object[]) => {
       .then(async (db: any) => {
         await db.collection("countries").drop();
         await db.collection("users").drop();
-        return db;
-      })
       // 2. create collections
-      .then(async (db: any) => {
         await db.createCollection("countries");
         await db.createCollection("users");
-        return db;
-      })
       // 3. populate collections - country data & user data
-      .then(async (db: any) => {
         await db.collection("countries").insertMany(countryData);
         await db.collection("users").insertMany(userData);
         return db;
